462ba268173029cd600cea90721d7f9f6484a059
f0rb1dden <38860149+f0rb1dden@users.noreply.github.com>
2018-05-09 15:50:14 +0100

    Add files via upload

---
+++ jscript/java-master (1)/java-master/Javascriptt/resources/script/main.js
@@ -0,0 +1,66 @@
+//window.alert("testing to see if linked");
+
+//console.log("testing to see if linked");
+
+// var is the declaration of a variable
+
+//"name" is what we have decided to call it
+//var name = "Max", job, salary;
+//variable is a container that stores data
+//job= "Developer"
+//salary= 25000;
+// Data types for JavaScript - numbers, strings, boolean, undefined and null
+
+//console.log(job); 
+
+//var 1student; //cannot start name with a number
+//var _1student; // undescore can be added before the number
+
+//var student; // javascript is case sensitive
+//var Student;
+
+
+//window.alert("HELLO WORLD");
+//alert(document.getElementById("demo"))("HELLO WORLD");
+
+//document.getElementById("demo").innerHTML = "HELLO WORLD";
+
+
+///////////////////////// DECLARING A VAR TO HOLD THE INPUT
+
+//var event1 = document.getElementById("button1");
+
+//SETUP AN EVENTLISTENER ON BUTTON1 CLICK
+
+//event1.addEventListene('click',functionName);
+
+//function functionName(){
+   // window.alert("is it workin");
+//WE ARE STORING THE INFORMATION ENTERED BY THE USER    
+//var userEntered = document.getElementById("enter").value;
+    
+//document.getElementById("output").innerHTML = userEntered;
+      
+
+    //event2.addEventListener("click")
+    //var letter1
+    
+    
+    var firstnumber
+    var secondnumber
+    var total = firstnumber + secondnumber; =document.getElementById("")
+    var userEntered = document.getElementById("enter").value;
+    
+
+    
+    
+    
+    
+    
+    
+    
+
+    
+
+    
+}
